1348574ce89fca92ca85a308e5158e7866f7feb0
Ted Roddy <me@tedroddy.net>
2025-04-28 20:24:23 -0400

    fix footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -9,7 +9,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>&copy; {new Date().getFullYear()} Ted Charles. All images are licensed under the Creative Commons Attribution 4.0 International License.</p>
+        <span>&copy; {new Date().getFullYear()} Ted Charles.</span>
         <div className="footer-links">
           <Link to="/license" className="footer-link">View License</Link>
         </div>
