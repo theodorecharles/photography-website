9a40d4ad8a64b1da57ffadd7937e5572154883c1
Ted Charles <me@tedcharles.net>
2025-11-11 19:20:28 -0500

    Fix unused setHasToasterAnimated in SSEToaster

--- frontend/src/components/AdminPortal/ConfigManager.tsx
+++ frontend/src/components/AdminPortal/ConfigManager.tsx
@@ -14,6 +14,7 @@ import {
   trackExternalLinksUpdate,
 } from "../../utils/analytics";
 import { PasswordInput } from "./PasswordInput";
+import { useSSEToaster } from "../../contexts/SSEToasterContext";
 
 const API_URL = import.meta.env.VITE_API_URL || "";
 
